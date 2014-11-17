df8cd96f7a729e520d5343251cd7db7418c152f3
Cihangir <chiryoku@gmail.com>
2014-11-17 15:26:31 -0500

    Give player option to load file only if save file exists

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,10 +1,14 @@
 require './lib/game.rb'
 
 if __FILE__ == $PROGRAM_NAME
-  puts "Load game? (y/n)"
-  option = gets.chomp
-  if option == "y"
-    game = Minesweeper.load
+  if File.exists?('./save_file')
+    puts "Load game? (y/n)"
+    option = gets.chomp
+    if option == "y"
+      game = Minesweeper.load
+    else
+      game = Minesweeper.new
+    end
   else
     game = Minesweeper.new
   end
